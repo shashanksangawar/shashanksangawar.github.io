e21e9b32908fc61211e9d6d17053c09ff9a6fe82
shashanksangawar <144597571+shashanksangawar@users.noreply.github.com>
2023-11-05 21:16:41 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-// toggle menu navbar
-let menuicon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar')
-
-menuicon.onclick = () =>
-{
-    menuicon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-} 
-
-
-
-
-
-// scroll Sections
-let sections=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
-window.onscroll = () =>
-{   
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let height = sec.offsetHeight;
-        let offset = sec.offsetTop - 100;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset+height)
-        {
-            navlinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
-            });
-            
-        }
-    });
-    let header=document.querySelector('header');
-    header.classList.toggle('sticky' , window.scrollY>100)
-
-    menuicon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-    
-=======
-// import { help, command_execution } from "./command";
 const outputElement = document.getElementById('output-container');
 const inputElement = document.getElementById('input');
 const inputContainer = document.getElementById('input-container');
@@ -206,5 +163,4 @@ function command_execution(command)
         default:
             return ""
     }
->>>>>>> c918269 (Second Commit)
 }
